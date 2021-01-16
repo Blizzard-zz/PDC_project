d7a2118599b26d26486c8a0a0a33d63b4f5c376c
zzz <747875693@qq.com>
2021-01-16 23:31:45 +0800

    2021.1.16.2

--- src/database/test_database.java
+++ src/database/test_database.java
@@ -5,11 +5,5 @@ import database.table.Customer;
 public class test_database {
     public static void main(String[] args) {
         new create_table();
-
-//        connection connection = new connection();
-
-//        Customer customer = new Customer()
-
-
     }
 }
